d47b483ad311a097ec4cdc41dbae86097ae83270
faizkur <mfaizk26@gmail.com>
2022-12-05 23:20:24 +0700

    third commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FE8C12",
+        secondary: "rgb(105 105 105)",
+        third:"rgb(255 255 255)",
+      }
+    },
+  },
+  plugins: [],
+}
